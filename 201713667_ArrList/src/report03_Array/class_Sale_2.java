2c9b3a8132c8871d59dbe71390be2371be34ecdb
pongja <rlqjadl2222@naver.com>
2020-04-06 21:12:40 +0900

    글자깨짐 수정

--- 201713667_ArrList/src/report03_Array/class_Sale_2.java
+++ 201713667_ArrList/src/report03_Array/class_Sale_2.java
@@ -6,7 +6,7 @@ public class class_Sale_2 {
         int sale[][] = new int[][] {{63,84,140,130}, {157,209,251,312}};
 
 		for(int i=0; i<2; i++) {
-			System.out.printf("<< %d�� >> \n", i+1);
+			System.out.printf("<< %d팀 >> \n", i+1);
 			for(int j=0; j<4; j++)
 				System.out.printf("%d/4분기: sale[%d][%d]= %d%n", j+1,i,j,sale[i][j]);
 				System.out.println();
